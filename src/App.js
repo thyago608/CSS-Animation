92934d0aa9a5c481bd890c5732c771c5aff29dd4
Thyago Ribeiro <70301260+thyago608@users.noreply.github.com>
2021-05-23 21:08:05 -0300

    Criação da estrutura

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div id="modal" className="animar">
+        <h1>Login</h1>
+        <label>Nome</label>
+        <input type="text" />
+        <label>Email</label>
+        <input type="text" />
+        <button>Logar</button>
+      </div>
+      <div id="load"></div>
+    </>
   );
 }
